8ee1d11bc7b99cf72f7e34f698ad54612ac815e6
Thays <thayscasado@hotmail.com>
2021-01-05 16:33:09 -0800

    ch01e07

---
+++ Chapter01/JavaBookExercise/src/MovieQuoteInfo.java
@@ -0,0 +1,28 @@
+
+public class MovieQuoteInfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 *  Write, compile, and test a class that displays your favorite movie quote, the movie
+			it comes from, the character who said it, and the year of the movie. Save the class
+			as MovieQuoteInfo.java
+		 */
+		
+		System.out.println("I hate the way you talk to me, and the way you cut your hair.");
+		System.out.println("I hate the way you drive my car. I hate it when you stare.");
+		System.out.println("I hate your big dumb combat boots, and the way you read my mind.");
+		System.out.println("I hate you so much it makes me sick; it even makes me rhyme.");
+		System.out.println("I hate it, I hate the way you're always right.");
+		System.out.println("I hate it when you lie. ");
+		System.out.println("I hate it when you make me laugh, even worse when you make me cry.");
+		System.out.println("I hate it when you're not around, and the fact that you didn't call.");
+		System.out.println("But mostly I hate the way I don't hate you.");
+		System.out.println("Not even close, not even a little bit, not even at all.");
+		System.out.println("");
+		
+		
+		System.out.println(" - Katarina Stratford, '10 THINGS I HATE ABOUT YOU'");
+	}
+
+}
