70f67d61eb6c9990bf1fada25bd96d569b4c0e3c
Thays <thayscasado@hotmail.com>
2021-01-05 20:30:27 -0800

    ch01e10

---
+++ Chapter01/JavaBookExercise/src/Comments.java
@@ -0,0 +1,45 @@
+import javax.swing.JOptionPane;
+
+public class Comments {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 *  Write, compile, and test a class
+			that uses the command window to
+			display the following statement about
+			comments: Program comments are
+			nonexecuting statements you add to a
+			file for documentation.
+			
+			Also include the same statement in
+			three different comments in the class;
+			each comment should use one of the
+			three different methods of including
+			comments in a Java class. Save the
+			class as Comments.java.
+		 */
+		JOptionPane.showMessageDialog(null, " Program comments are nonexecuting \r\n"
+				+ "   statements you add to file for a\r\n"
+				+ "           documentation.");
+		/*
+		 * Block comments
+		 *  Program comments are
+			nonexecuting statements you add to a
+			file for documentation
+		 */
+		
+		// Line Comments
+		// Program comments are
+		// nonexecuting statements you add to a
+		// file for documentation
+		
+		/**
+		 * Javadoc
+		 *  Program comments are
+			nonexecuting statements you add to a
+			file for documentation
+		 */
+	}
+
+}
