3dfc43949fdc44d020f5d227c3897a507105fb9d
Thays <thayscasado@hotmail.com>
2021-01-05 20:49:46 -0800

    ch01e12

---
+++ Chapter01/JavaBookExercise/src/BrumaShave.java
@@ -0,0 +1,30 @@
+import javax.swing.JOptionPane;
+public class BrumaShave {
+
+	public static void main(String[] args) {
+		// TODO
+		/*
+		 * From 1925 through 1963, Burma Shave advertising signs appeared next to
+highways all across the United States. There were always four or five signs in
+a row containing pieces of a rhyme, followed by a final sign that read “Burma
+Shave.” For example, one set of signs that has been preserved by the Smithsonian
+Institution reads as follows:
+Shaving brushes
+You'll soon see 'em
+On a shelf
+In some museum
+Burma Shave
+Find a classic Burma Shave rhyme on the Web. Write, compile, and test a class
+that produces a series of four dialog boxes so that each displays one line of a
+Burma Shave slogan in turn. Save the class as BurmaShave.java.
+		 */
+
+		JOptionPane.showMessageDialog(null, "THE WOLF");
+		JOptionPane.showMessageDialog(null, "IS SHAVED");
+		JOptionPane.showMessageDialog(null, "SO NEAT AND TRIM");
+		JOptionPane.showMessageDialog(null, "RED RIDING HOOD");
+		JOptionPane.showMessageDialog(null, "IS CHASING HIM");
+		JOptionPane.showMessageDialog(null, "/ BurmaShave");
+	}
+
+}
