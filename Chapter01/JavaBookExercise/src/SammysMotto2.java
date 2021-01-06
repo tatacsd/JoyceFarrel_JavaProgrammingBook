3ccc156f34dfe1ee8333895a58bd33eecf623acc
Thays <thayscasado@hotmail.com>
2021-01-05 21:48:23 -0800

    CaseProblem02

---
+++ Chapter01/JavaBookExercise/src/SammysMotto2.java
@@ -0,0 +1,24 @@
+import javax.swing.JOptionPane;
+
+public class SammysMotto2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Dialog Box
+		JOptionPane.showMessageDialog(null, "SsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSs\n"
+				+ "Ss                                                                                 Ss\n"
+				+ "Ss            Sammy makes it fun in the sun.            Ss\n"
+				+ "Ss                                                                                 Ss\n"
+				+ "SsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSs\n");
+		
+		
+		// Console
+		System.out.println("SsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSs");
+		System.out.println("Ss                                                  Ss");
+		System.out.println("Ss    Sammy makes it fun in the sun.                Ss");
+		System.out.println("Ss                                                  Ss");
+		System.out.println("SsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSs");
+
+	}
+
+}
