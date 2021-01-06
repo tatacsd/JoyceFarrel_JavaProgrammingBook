f5c6fb423fa66a71cd9d18c81371780ba073a1fe
Thays <thayscasado@hotmail.com>
2021-01-05 16:15:25 -0800

    ch01e06

---
+++ Chapter01/JavaBookExercise/src/SongLyrics.java
@@ -0,0 +1,12 @@
+
+public class SongLyrics {
+
+	public static void main(String[] args) {
+		// TODO 
+		// Write, compile, and test a class that displays the first few lines of the lyrics of your
+		// favorite song. Save the class as SongLyrics.java
+		
+		System.out.println("Quando o mel é bom, a abelha sempre volta ..");
+		System.out.println("By Coleguinhas - Simone e Simaria");
+	}
+}
