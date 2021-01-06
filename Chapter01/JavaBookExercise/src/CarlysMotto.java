8a2567f63c5e809b7575aecea0d5ace26395bdb4
Thays <thayscasado@hotmail.com>
2021-01-05 21:37:41 -0800

    CaseProblem01

---
+++ Chapter01/JavaBookExercise/src/CarlysMotto.java
@@ -0,0 +1,10 @@
+
+public class CarlysMotto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Carly makes the food that makes it party.");
+
+	}
+
+}
---
+++ Chapter01/JavaBookExercise/src/CarlysMotto2.java
@@ -0,0 +1,26 @@
+import javax.swing.JOptionPane;
+
+public class CarlysMotto2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Dialog Box
+		JOptionPane.showMessageDialog(null, "**********************************************************\n"
+				+ "**                                                                                 **\n"
+				+ "**     Carly makes the food that makes it party.     **\n"
+				+ "**                                                                                 **\n"
+				+ "**********************************************************\n");
+		
+		
+		// Console
+		System.out.println("*****************************************************");
+		System.out.println("**                                                 **");
+		System.out.println("**    Carly makes the food that makes it party.    **");
+		System.out.println("**                                                 **");
+		System.out.println("*****************************************************");
+
+
+	}
+
+}
