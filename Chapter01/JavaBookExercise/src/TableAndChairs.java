63c7f62486a6eedc9645581862e0b2d193b00a2d
Thays <thayscasado@hotmail.com>
2021-01-05 16:41:10 -0800

    ch01e08

---
+++ Chapter01/JavaBookExercise/src/TableAndChairs.java
@@ -0,0 +1,22 @@
+
+public class TableAndChairs {
+
+	public static void main(String[] args) {
+		/*
+		 *  Write, compile, and test a class that
+			displays the pattern shown in
+			Figure 1-25. Save the class as
+			TableAndChairs.java.
+		 */
+		
+		
+		System.out.println("X                      X");
+		System.out.println("X                      X");
+		System.out.println("X      XXXXXXXXXX      X");
+		System.out.println("XXXXX  XXXXXXXXXX  XXXXX");
+		System.out.println("X   X  XXXXXXXXXX  X   X");
+		System.out.println("X   X  XXXXXXXXXX  X   X");
+
+	}
+
+}
