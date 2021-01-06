d031bd6c19fbe84ac19470a85160c00345d4531c
Thays <thayscasado@hotmail.com>
2021-01-05 20:35:59 -0800

    ch01e11

--- Chapter01/JavaBookExercise/src/CommentsDialog.java
+++ Chapter01/JavaBookExercise/src/CommentsDialog.java
@@ -4,6 +4,13 @@ public class CommentsDialog {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		/*
+		 * Modify the Comments.java program
+			in Exercise 10 so that the statement
+			about comments is displayed in a
+			dialog box. Save the class as
+			CommentsDialog.java
+					 */
 		JOptionPane.showMessageDialog(null, " Program comments are nonexecuting \r\n"
 				+ "   statements you add to file for a\r\n"
 				+ "           documentation.");
