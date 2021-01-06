1676ad0eb69a33027b738150c433ce9107ae0635
Thays <thayscasado@hotmail.com>
2021-01-05 20:34:59 -0800

    ch01e11

---
+++ Chapter01/JavaBookExercise/src/CommentsDialog.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class CommentsDialog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, " Program comments are nonexecuting \r\n"
+				+ "   statements you add to file for a\r\n"
+				+ "           documentation.");
+
+	}
+
+}
