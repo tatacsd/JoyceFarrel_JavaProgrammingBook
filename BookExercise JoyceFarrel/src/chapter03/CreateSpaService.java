08b6fd82b754b7fcd2192a46bbb47e110b1b4c01
Thays <thayscasado@hotmail.com>
2021-01-22 15:46:19 -0800

    ch03

--- BookExercise JoyceFarrel/src/chapter03/CreateSpaService.java
+++ BookExercise JoyceFarrel/src/chapter03/CreateSpaService.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 
 public class CreateSpaService {
 	public static void main(String[] args) {
-		
-		
 //		// Declare variables
 //		String service;
 //		double price;
@@ -15,30 +13,8 @@ public class CreateSpaService {
 		SpaService secondService = new SpaService();
 		
 		firstService = getData(firstService);
-		secondService = getData(secondService);		
-		
-		// Initialize Scanner
-//		Scanner keyboard = new Scanner(System.in);
-//		System.out.print("Enter the service >> ");
-//		service = keyboard.nextLine();
-//		System.out.print("Enter price >> ");
-//		price =  keyboard.nextDouble();
-//		
-//		// Set the service and price value to the object using the set method
-//		firstService.setServiceDescription(service);
-//		firstService.setPrice(price);
-		
-//		keyboard.nextLine();
-//		
-//		System.out.print("Enter the service >> ");
-//		service = keyboard.nextLine();
-//		System.out.print("Enter price >> ");
-//		price =  keyboard.nextDouble();
-//		
-//		secondService.setServiceDescription(service);
-//		secondService.setPrice(price);
-//		
-//		keyboard.close();
+		// The constructor will add the info
+		// secondService = getData(secondService);		
 		
 		System.out.printf("First service details:\n%s $%.2f.\n", firstService.getServiceDescription(), firstService.getPrice());
 		System.out.printf("Second service details:\n%s $%.2f.", secondService.getServiceDescription(), secondService.getPrice());
@@ -46,6 +22,7 @@ public class CreateSpaService {
 	}
 	
 	// Methods
+	
 	// method that returns an object of type SpaService and as a parameter has SpaService object
 	public static SpaService getData(SpaService serviceObj) {
 		// Declare variables
@@ -63,7 +40,7 @@ public class CreateSpaService {
 		serviceObj.setPrice(price);
 		
 		keyboard.nextLine();
-		return serviceObj;			
-		
+		// keyboard.close(); // I can't close here because an error occur
+		return serviceObj;
 	}
 }
---
+++ BookExercise JoyceFarrel/src/chapter03Exercise/NumbersDemo.java
@@ -0,0 +1,19 @@
+package chapter03Exercise;
+
+public class NumbersDemo {
+	public static void main(String[] args) {
+		int age = 32;
+		int weight = 180;
+		double height = 5.9;
+		
+		calculate(weight, weight);
+		
+		System.out.println(weight);
+	}
+	
+	public static void calculate(int age, double size) {
+		double one = size;
+		int age = age;
+	}
+
+}
