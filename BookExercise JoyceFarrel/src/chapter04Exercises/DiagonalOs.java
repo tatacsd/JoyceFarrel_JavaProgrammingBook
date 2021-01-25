775c3eb0f681e594b596d203eed8b21db01cd95b
Thays <thayscasado@hotmail.com>
2021-01-24 21:48:02 -0800

    CH04Q06

---
+++ BookExercise JoyceFarrel/src/chapter04Exercises/DiagonalOs.java
@@ -0,0 +1,14 @@
+package chapter04Exercises;
+public class DiagonalOs {
+	public static void main(String[] args) {
+		int row = 10;
+		for(int i = 0; i < row; i++) {
+			// Spaces
+			for(int j = 1; j <= i; j++) {
+				System.out.print(" ");				
+			}				
+			// Os
+			System.out.println("0");
+		}
+	}
+}
