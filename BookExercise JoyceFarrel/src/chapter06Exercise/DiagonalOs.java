9135c65a1e28b84992f803a90e453b8749441b46
Thays <thayscasado@hotmail.com>
2021-01-24 21:57:44 -0800

    ch06q06

--- BookExercise JoyceFarrel/src/chapter06Exercise/DiagonalOs.java
+++ BookExercise JoyceFarrel/src/chapter06Exercise/DiagonalOs.java
@@ -1,5 +1,13 @@
 package chapter06Exercise;
+/*
+ * 6. Write an application that uses a loop to create the
+pattern of Os shown in Figure 6-32, in which each O
+is displayed one additional space to the right. Save
+the file as DiagonalOs.java
+ */
+
 public class DiagonalOs {
+	
 	public static void main(String[] args) {
 		int row = 10;
 		for(int i = 0; i < row; i++) {
@@ -7,7 +15,7 @@ public class DiagonalOs {
 			for(int j = 1; j <= i; j++) {
 				System.out.print(" ");				
 			}				
-			// Os
+			// O's
 			System.out.println("0");
 		}
 	}
