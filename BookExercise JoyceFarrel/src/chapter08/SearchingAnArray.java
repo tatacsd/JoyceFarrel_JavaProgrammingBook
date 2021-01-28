c09a6774f15cef28848f19b872af4485efea440d
Thays <thayscasado@hotmail.com>
2021-01-28 01:01:17 -0800

    ch08

---
+++ BookExercise JoyceFarrel/src/chapter08/SearchingAnArray.java
@@ -0,0 +1,20 @@
+package chapter08;
+
+public class SearchingAnArray {
+	public static void main(String[] args) {
+		
+		// Declare and Initialize an array with valid values 
+		int[] validValues = {101, 108, 201, 213, 266, 304, 311, 409, 411, 412};
+		
+		// Loop through array
+		for(int i = 0; i < validValues.length; i++) {
+			//  Finding the item number that matches
+			if(itemOrdered == validValues[i]) {
+				validItem = true;
+			}
+		}
+		
+				
+		
+	}
+}
