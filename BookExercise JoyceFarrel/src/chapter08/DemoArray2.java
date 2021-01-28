d9926e2e5a657662dc5598255426ffb11c1ec0d0
Thays <thayscasado@hotmail.com>
2021-01-28 01:00:47 -0800

    ch08

---
+++ BookExercise JoyceFarrel/src/chapter08/DemoArray2.java
@@ -0,0 +1,25 @@
+package chapter08;
+
+public class DemoArray2 {
+	public static void main(String[] args) {
+		
+		// Declare and create an array for double
+		// And Initialize (POPULATE THE ARRAY) it.
+		double[] salaries = {12.25, 13.55, 14.25, 16,85};
+		
+	
+		
+		// Display the salaries one by one
+		System.out.println("Salaries one by one are:");
+		System.out.println(salaries[0]);
+		System.out.println(salaries[1]);
+		System.out.println(salaries[2]);
+		System.out.println(salaries[3]);
+		
+		// Using subscription that is out of bound
+		//System.out.println(salaries[4]);
+		
+		
+	}
+
+}
