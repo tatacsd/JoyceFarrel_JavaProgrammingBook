f8d8b456b1d1bb4a8125044d811748444fc53eab
Thays <thayscasado@hotmail.com>
2021-02-08 23:39:26 -0800

    Chapter 07 Exercises

---
+++ BookExercise JoyceFarrel/src/chapter07Exercise/CountWords.java
@@ -0,0 +1,32 @@
+package chapter07Exercise;
+/*
+			 * 5. Write an application that counts the words in a String entered by a user. Words
+			are separated by any combination of spaces, periods, commas, semicolons,
+			question marks, exclamation points, or dashes. Figure 7-17 shows a typical
+			execution. Save the file as CountWords.java
+ */
+
+import java.util.Scanner;
+
+public class CountWords {
+	
+	public static void main(String[] args) {
+//		
+//		String str;
+//		
+//		char[] delimiter = {' ', '.', ',', ';', '?', '!', '-'};
+//		String delimitrs ="\\s+"
+//		
+//		// Initialize the scanner
+//		Scanner keyboard = new Scanner(System.in);
+//		System.out.print("Enter a string >> ");
+//		str = keyboard.nextLine();
+//		String[] words = str.split(' ', '.', ',', ';', '?', '!', '-');
+//		
+//		
+//		
+//		// Close scanner
+//		keyboard.close();
+	}
+
+}
