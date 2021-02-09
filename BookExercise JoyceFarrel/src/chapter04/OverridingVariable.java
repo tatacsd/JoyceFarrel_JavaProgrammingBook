ced5e2165b46157c9ff18fbea8572f7dba709cd3
Thays <thayscasado@hotmail.com>
2021-02-08 23:33:22 -0800

    Chapter 4

---
+++ BookExercise JoyceFarrel/src/chapter04/OverridingVariable.java
@@ -0,0 +1,33 @@
+package chapter04;
+
+public class OverridingVariable {
+	public static void main(String[] args) {
+		int aNumber = 10;
+		System.out.println("In main(), aNumber is " + aNumber);
+		
+		firstMethod();
+		System.out.println("Back in main(), aNumber is " + aNumber);
+		
+		secondMethod(aNumber);
+		System.out.println("Back in main() again, aNumber is " + aNumber);
+		
+		
+		
+		
+	
+		
+	}
+	
+	//Methods
+	public static void firstMethod() {
+		int aNumber = 77;
+		System.out.println("In firstMethod(), aNumber is " + aNumber);
+	}
+	
+	public static void secondMethod(int aNumber) {
+		System.out.println("In secondMethod(), at first aNumber is " + aNumber);
+		aNumber = 77;
+		System.out.println("In secondMethod(), after an assignment aNumber is " + aNumber);
+		
+	}
+}
