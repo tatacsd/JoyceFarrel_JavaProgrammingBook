9151e8aa480b1b0f0513f303f1ff45ad25c36e0c
Thays <thayscasado@hotmail.com>
2021-02-08 23:42:51 -0800

    chapter 08

---
+++ BookExercise JoyceFarrel/src/chapter08/BowlingTeam.java
@@ -0,0 +1,30 @@
+package chapter08;
+
+public class BowlingTeam {
+	
+	// Field for teamName and an array to hold it
+	private String teamName;
+	private String[] members = new String[4];
+	
+	
+	// Setters and getters
+	public String getTeamName() {
+		return teamName;
+	}
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+
+	// Methods
+	// Set the team members name
+	public void setMember(int number, String name) {
+		members[number] = name;
+	}
+	
+	// Get team members name
+	public String getMember(int number) {
+		return members[number];
+	}
+	
+
+}
