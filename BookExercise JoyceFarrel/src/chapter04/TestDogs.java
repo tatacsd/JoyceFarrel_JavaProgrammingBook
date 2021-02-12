27a8fed3de87531525d4e23126d7a67501828e83
Thays <thayscasado@hotmail.com>
2021-02-11 21:47:28 -0800

    Chapter04

---
+++ BookExercise JoyceFarrel/src/chapter04/TestDogs.java
@@ -0,0 +1,18 @@
+package chapter04;
+
+public class TestDogs {
+	
+	public static void main(String[] args) {
+		DogTriathlonParticipant dog1 = new DogTriathlonParticipant("Bowser", 2, 85, 89, 0);
+		
+		dog1.display();
+		
+		DogTriathlonParticipant dog2 = new DogTriathlonParticipant("Rush", 3, 78, 72, 80);
+		DogTriathlonParticipant dog3 = new DogTriathlonParticipant("Ginger", 3, 90, 86, 72);
+		
+		
+		dog2.display();
+		dog3.display();
+	}
+
+}
