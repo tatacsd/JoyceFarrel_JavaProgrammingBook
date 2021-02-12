520dc04e84acafda73eb1bc137e865b1cc705dc3
Thays <thayscasado@hotmail.com>
2021-02-11 21:46:30 -0800

    Chapter04

---
+++ BookExercise JoyceFarrel/src/chapter04/CreatePolicies.java
@@ -0,0 +1,17 @@
+package chapter04;
+
+public class CreatePolicies {
+	public static void main(String[] args) {
+		CarInsurancePolicy first = new CarInsurancePolicy(123);
+		CarInsurancePolicy second = new CarInsurancePolicy(456, 4);
+		CarInsurancePolicy third = new CarInsurancePolicy(789, 12, "Newcastle");
+		
+		first.display();
+		second.display();
+		third.display();
+		
+		CarInsurancePolicy fourth = new CarInsurancePolicy();
+		
+		fourth.display();
+	}
+}
