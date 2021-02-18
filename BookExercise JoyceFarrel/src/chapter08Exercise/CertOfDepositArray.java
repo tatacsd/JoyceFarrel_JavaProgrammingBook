bf24d993b00aaa9d70911a5186ad9d168567854a
Thays <thayscasado@hotmail.com>
2021-02-17 21:40:00 -0800

    chapter 08 exercise

---
+++ BookExercise JoyceFarrel/src/chapter08Exercise/CertOfDepositArray.java
@@ -0,0 +1,10 @@
+package chapter08Exercise;
+/*
+			 * 10. In the exercises in Chapter 4, you created a CertOfDeposit class. Now, create an
+			application to accept data for an array of five CertOfDeposit objects, and then
+			display the data. Save the application as CertOfDepositArray.java
+ */
+
+public class CertOfDepositArray {
+
+}
