bfe901e44010def3f820515a32b0741d29637c78
Thays <thayscasado@hotmail.com>
2021-02-22 01:15:48 -0800

    chapter09

---
+++ BookExercise JoyceFarrel/src/chapter09/Employee.java
@@ -0,0 +1,37 @@
+package chapter09;
+
+public class Employee {
+	// Data field
+	private int empNum;
+	private String lastName;
+	private String firstName;
+	private double salary;
+
+	
+	// Setters and getters
+	public int getEmpNum() {
+		return empNum;
+	}
+	public void setEmpNum(int empNum) {
+		this.empNum = empNum;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+}
